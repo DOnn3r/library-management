30aab7cdb83ee329a6019404e232e5d17165bdaf
Donner <152691192+DOnn3r@users.noreply.github.com>
2024-12-04 18:01:24 +0300

    first commit

---
+++ src/main/java/org/example/libraymanagement/LibrayManagementApplication.java
@@ -0,0 +1,13 @@
+package org.example.libraymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrayManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibrayManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/libraymanagement/LibrayManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.libraymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrayManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
